32c580399d562f15abc02fa22e4d58714dc2ca7a
Benjamin Wölfle <133773693+benjaminUni@users.noreply.github.com>
2023-05-17 20:06:03 +0200

    Ergebnisanzeige hinzugefügt
    Ergebnisanzeige ui element wurde der Oberfläche hinzugefügt

--- gender-automat/src/app/app-nav/app-nav.component.ts
+++ gender-automat/src/app/app-nav/app-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
@@ -9,7 +9,7 @@ import { map, shareReplay } from 'rxjs/operators';
   styleUrls: ['./app-nav.component.css']
 })
 export class AppNavComponent {
-  
+
   private breakpointObserver = inject(BreakpointObserver);
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
